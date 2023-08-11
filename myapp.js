df85d0983e499b33588edc5e0a1542dcbf6c477d
Bruna <verronebruna@gmail.com>
2023-08-11 16:13:54 -0300

    [RandomCodes] use express

---
+++ myapp.js
@@ -0,0 +1,11 @@
+const express = require('express'); // import express
+const app = express(); // create express app
+const port = 3000; // here we defined the port and we will use it later
+
+app.get('/route/', (req, res) => { // here we defined the route and the callback function
+    res.send('Hello World!'); // here we send the response
+}); 
+
+app.listen(port, () => { // here we start the server
+    console.log(`Example app listening at http://localhost:${port}`); // here we log the port
+});
